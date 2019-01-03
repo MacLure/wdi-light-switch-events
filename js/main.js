e21c63a2cf716c0874f1ca5600a6d83a3acba49c
Malcolm <malcolm@Malcolms-MacBook-Pro.local>
2019-01-03 14:27:40 -0500

    complete the assignment

--- js/main.js
+++ js/main.js
@@ -1,2 +1,27 @@
+document.addEventListener('DOMContentLoaded', function() {
+    
+    const lightswitch = document.getElementsByTagName('button')[0];
+
+    lightswitch.addEventListener('click', toggleLights)
+    
+    function toggleLights() {
+        const text = document.getElementsByTagName('h1')[0];
+        const body = document.getElementsByTagName('body')[0];
+    
+        if (body.classList.contains("light")) {
+            body.classList.remove("light")
+            body.classList.add("dark")
+            text.innerText = "Hey, who turned off the lights?";
+    
+        } else {
+            body.classList.remove("dark")
+            body.classList.add("light")
+            text.innerText = "It's so bright in here!";
+        }
+    }
+})
+
+
+
 
 
