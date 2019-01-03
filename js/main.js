d8bd10c0fdd9569f9a40380150626ecc1715474a
Malcolm <malcolm@Malcolms-MacBook-Pro.local>
2019-01-03 14:49:44 -0500

    fixed lightswitch class issue

--- js/main.js
+++ js/main.js
@@ -9,14 +9,19 @@ document.addEventListener('DOMContentLoaded', function() {
         const body = document.getElementsByTagName('body')[0];
     
         if (body.classList.contains("light")) {
-            body.classList.remove("light")
-            body.classList.add("dark")
+            body.classList.remove("light");
+            body.classList.add("dark");
             text.innerText = "Hey, who turned off the lights?";
+            lightswitch.classList.remove("on");
+            lightswitch.classList.add("off");
     
         } else {
-            body.classList.remove("dark")
-            body.classList.add("light")
+            body.classList.remove("dark");
+            body.classList.add("light");
             text.innerText = "It's so bright in here!";
+            lightswitch.classList.add("on");
+            lightswitch.classList.remove("off");
+
         }
     }
 })
